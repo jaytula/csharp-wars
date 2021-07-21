68a830aa1a7f63080f47a2ec0542a0e7e828ea2a
Jay T <jaytula@gmail.com>
2021-07-20 17:52:06 -0700

    Finish 'The Wide-Mouthed Frog'

---
+++ WideMouthedFrog/Class1.cs
@@ -0,0 +1,7 @@
+﻿public class Kata
+{
+  public static string MouthSize(string animal)
+  {
+    return animal.ToLower() == "alligator" ? "small" : "wide";
+  }
+}
---
+++ WideMouthedFrogTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Solution
+{
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class SolutionTest
+  {
+    [Test]
+    public void SampleTest()
+    {
+      Assert.AreEqual("wide", Kata.MouthSize("toucan"));
+      Assert.AreEqual("wide", Kata.MouthSize("ant bear"));
+      Assert.AreEqual("small", Kata.MouthSize("alligator"));
+    }
+  }
+}
