66c321960756868221562a4c81341c17ef1e38e2
Jay T <jaytula@gmail.com>
2021-07-20 23:09:12 -0700

    Finish 'Square(n) Sum'

---
+++ SquareSum/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static int SquareSum(int[] n)
+  {
+    return (int)n.Aggregate(0, (acc, x) => acc + (int)Math.Pow((int)x, 2));
+  }
+}
