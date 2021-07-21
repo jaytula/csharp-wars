9d3d70431faa2708ca8bd0d55884f2202cf84340
Jay T <jaytula@gmail.com>
2021-07-21 15:42:29 -0700

    Complete 'Compare within margin'

---
+++ CompareWithinMargin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Kata
+{
+  public static int CloseCompare(double a, double b, double margin = 0)
+  {
+    double difference = a - b;
+
+    if(Math.Abs(difference) <= margin) return 0;
+    return difference < 0 ? -1 : 1;
+  }
+}
