a4727e556307b7fdb9799083dcbfb70d52d962c6
Jay T <jaytula@gmail.com>
2021-07-21 15:54:22 -0700

    Finish 'Get the mean of an array'

---
+++ GetMeanOfArray/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+public class Kata
+{
+  public static int GetAverage(int[] marks)
+  {
+    int total = marks.Aggregate(0, (acc, curr) => acc + curr);
+    return (int)Math.Floor((double)total / marks.Length);
+  }
+}
---
+++ GetMeanOfArrayTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+public class Tests
+{
+  [Test]
+  [TestCase(new int[]{2,2,2,2}, ExpectedResult= 2)]
+  [TestCase(new int[]{5,10}, ExpectedResult= 7)]
+  public static int FixedTest(int[] marks)
+  {
+    return Kata.GetAverage(marks);
+  }
+}
