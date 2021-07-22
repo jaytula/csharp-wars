60e2d4338ddd965bfb8c69ebc7638fd7f72db182
Jay T <jaytula@gmail.com>
2021-07-22 16:52:17 -0700

    Finish 'Cat years, Dog years'

---
+++ CatYearsDogYears/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Dinglemouse
+{
+  public static int[] humanYearsCatYearsDogYears(int humanYears)
+  {
+    int catYears = (humanYears == 1 ? 15 : 24) + Math.Max(humanYears - 2, 0) * 4;
+    int dogYears = (humanYears == 1 ? 15 : 24) + Math.Max(humanYears - 2, 0) * 5;
+
+    return new int[] { humanYears, catYears, dogYears };
+  }
+
+}
