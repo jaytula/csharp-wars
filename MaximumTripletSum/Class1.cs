59ffdd4974d9df6ff169689b54862cee58a7652d
Jay T <jaytula@gmail.com>
2021-07-25 19:30:06 -0700

    Finish 'Maximum Triplet Sum'

---
+++ MaximumTripletSum/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Kata
+{
+  public static int MaxTriSum(int[] a)
+  {
+    var h = new HashSet<int>(a);
+    var arr = h.ToArray();
+
+    Array.Sort(arr);
+
+    if(arr.Length <= 3) return arr.Sum();
+    return new List<int>(arr).GetRange(arr.Length - 3, 3).ToArray().Sum();
+  }
+}
