ba53e4ba0c8dec5500dd1c453a80ab973337f69b
Jay T <jaytula@gmail.com>
2021-07-27 22:13:18 -0700

    Finish 'Unlucky Days'

---
+++ UnluckyDays/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Kata
+{
+  public static int UnluckyDays(int year)
+  {
+    int count = 0;
+
+    for (int i = 1; i <= 12; i++)
+    {
+      DateTime dt = new DateTime(year, i, 13);
+      if (dt.DayOfWeek == DayOfWeek.Friday) count++;
+    }
+    return count;
+  }
+}
