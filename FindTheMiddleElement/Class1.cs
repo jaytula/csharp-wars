71a709cc1a69e06f74d0967701c9aadfd8cefdc4
Jay T <jaytula@gmail.com>
2021-07-27 22:34:53 -0700

    Finish 'Find the middle element'

---
+++ FindTheMiddleElement/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+public class Kata
+{
+  public static int Gimme(double[] inputArray)
+  {
+    var copiedArray = new List<double>(inputArray).ToArray();
+    Array.Sort(copiedArray);
+
+    return Array.IndexOf(inputArray, copiedArray[1]);
+  }
+}
---
+++ FindTheMiddleElementTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Solution {
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class Test
+  {
+    [Test]
+    public void SampleTests()
+    {
+      Assert.AreEqual(0, Kata.Gimme(new double[] {2, 3, 1}));
+      Assert.AreEqual(1, Kata.Gimme(new double[] {5, 10, 14}));
+    }
+  }
+}
