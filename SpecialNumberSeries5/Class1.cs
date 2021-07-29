5da37c501f269a4a16174de9a4d85d9e17f7a332
Jay T <jaytula@gmail.com>
2021-07-28 23:30:43 -0700

    Finish 'Special Number (Special Numbers Series #5)'

---
+++ SpecialNumberSeries5/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+public class Kata
+{
+  public static string SpecialNumber(int number)
+  {
+    var isSpecial = number.ToString().Aggregate(true, (acc, x) => acc && (x >= '0' && x <= '5'));
+    return isSpecial ? "Special!!" : "NOT!!";
+  }
+}
