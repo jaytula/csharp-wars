8044a6b358a8d3351583e478f15e2e2888d22dcd
Jay T <jaytula@gmail.com>
2021-07-28 23:49:16 -0700

    Finish 'Jumping Number (Special Numbers Series #4)'

---
+++ JumpingNumber/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static string JumpingNumber(int number)
+    {
+      string s = number.ToString();
+
+      for(int i = 1; i < s.Length; i++) {
+        int prev = int.Parse(s[i-1].ToString());
+        int curr = int.Parse(s[i].ToString());
+        if(Math.Abs(prev-curr) != 1) {
+          return "Not!!";
+        }
+      }
+      return "Jumping!!";
+    }
+}
