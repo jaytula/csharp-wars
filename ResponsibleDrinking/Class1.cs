183d87d48cc6eb674bea5a1f276d48380379f6c9
Jay T <jaytula@gmail.com>
2021-07-28 23:19:37 -0700

    Finish 'Responsible Drinking'

--- ResponsibleDrinking/Class1.cs
+++ ResponsibleDrinking/Class1.cs
@@ -1,8 +1,18 @@
-﻿using System;
+﻿using System.Text.RegularExpressions;
 
-namespace ResponsibleDrinking
+public class Drinkin
 {
-    public class Class1
+  public string hydrate(string drinkString)
+  {
+    Regex regex = new Regex(@"\b([0-9])\b");
+    MatchCollection matches = regex.Matches(drinkString);
+    int result = 0;
+
+    for (int ctr = 0; ctr < matches.Count; ctr++)
     {
+      result += int.Parse(matches[ctr].Value);
     }
+
+    return $"{result} glass{(result > 1 ? "es" : "")} of water";
+  }
 }
--- ResponsibleDrinkingTests/UnitTest1.cs
+++ ResponsibleDrinkingTests/UnitTest1.cs
@@ -1,18 +1,30 @@
+using System;
 using NUnit.Framework;
 
-namespace ResponsibleDrinkingTests
+namespace CodewarsTests
 {
-    public class Tests
+
+    [TestFixture]
+    public class DrinkinTest
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void drinkinTest1()
         {
+            Drinkin drinkin = new Drinkin();
+            string expected = "1 glass of water";
+            string actual = drinkin.hydrate("1 beer");
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void Test1()
+        public void drinkinTest3()
         {
-            Assert.Pass();
+            Drinkin drinkin = new Drinkin();
+            string expected = "10 glasses of water";
+            string actual = drinkin.hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer");
+            Assert.AreEqual(expected, actual);
         }
+
     }
 }
